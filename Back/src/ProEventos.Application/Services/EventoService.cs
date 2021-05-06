30af5c0a7a3de6a16161a6172cb3dabbd27af461
alvesRoniel <roniel.alves@gmail.com>
2021-05-06 20:25:11 -0300

    Seção 6: .NET Core - P3 - Criação do Evento Serviço na camada de Aplicattion.

---
+++ Back/src/ProEventos.Application/Services/EventoService.cs
@@ -0,0 +1,126 @@
+using System;
+using System.Threading.Tasks;
+using ProEventos.Application.Interfaces;
+using ProEventos.Domain;
+using ProEventos.Persistence.Interfaces;
+
+namespace ProEventos.Application.Services
+{
+    public class EventoService : IEventoService
+    {
+        private readonly IGeralPersist _geralPersist;
+        private readonly IEventoPersist _eventoPersist;
+        public EventoService(IGeralPersist geralPersist, IEventoPersist eventoPersist)
+        {
+            _eventoPersist = eventoPersist;
+            _geralPersist = geralPersist;
+        }
+
+        public async Task<Evento> AddEventos(Evento model)
+        {
+            try
+            {
+                _geralPersist.Add<Evento>(model);
+                if (await _geralPersist.SaveChangesAsync())
+                {
+                    return await _eventoPersist.GetEventoByIdAsync(model.Id, false);
+                }
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<Evento> UpdateEventos(int eventoId, Evento model)
+        {
+            try
+            {
+                var evento = await _eventoPersist.GetEventoByIdAsync(eventoId, false);
+                if (evento == null)
+                    return null;
+
+                model.Id = evento.Id;
+
+                _geralPersist.Upadate(model);
+                if (await _geralPersist.SaveChangesAsync())
+                {
+                    return await _eventoPersist.GetEventoByIdAsync(model.Id, false);
+                }
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<bool> DeleteEventos(int eventoId)
+        {
+            try
+            {
+                var evento = await _eventoPersist.GetEventoByIdAsync(eventoId, false);
+                if (evento == null)
+                {
+                    throw new Exception("O Evento para remoção não foi encontrado.");
+                }
+
+                _geralPersist.Delete<Evento>(evento);
+                return await _geralPersist.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<Evento[]> GetAllEventosAsync(bool includePalestrante = false)
+        {
+            try
+            {
+                var eventos = await _eventoPersist.GetAllEventosAsync(includePalestrante);
+                if (eventos == null)
+                    return null;
+
+                return eventos;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<Evento[]> GetAllEventosByTemaAsync(string tema, bool includePalestrante = false)
+        {
+            try
+            {
+                var eventos = await _eventoPersist.GetAllEventosByTemaAsync(tema, includePalestrante);
+                if (eventos == null)
+                    return null;
+
+                return eventos;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<Evento> GetEventoByIdAsync(int eventoId, bool includePalestrante = false)
+        {
+            try
+            {
+                var eventos = await _eventoPersist.GetEventoByIdAsync(eventoId, includePalestrante);
+                if (eventos == null)
+                    return null;
+
+                return eventos;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}

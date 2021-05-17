65a9f6113d80bb69573d6d2ef628e92474e0c470
alvesRoniel <roniel.alves@gmail.com>
2021-05-17 19:42:33 -0300

    Seção 8: .NET Core - P4 - Angular - Adicionando as validações(Reactive Form) no forumário de cadastro de Eventos.

--- Front/ProEventos-App/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
+++ Front/ProEventos-App/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-evento-detalhe',
   templateUrl: './evento-detalhe.component.html',
-  styleUrls: ['./evento-detalhe.component.scss']
+  styleUrls: ['./evento-detalhe.component.scss'],
 })
 export class EventoDetalheComponent implements OnInit {
+  public frmEventoDetalhe!: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get f(): any {
+    return this.frmEventoDetalhe.controls;
   }
 
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.validation();
+  }
+
+  validation(): void {
+    this.frmEventoDetalhe = this.formBuilder.group({
+      tema: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(50),
+        ],
+      ],
+      local: ['', Validators.required],
+      dataHoraEvento: ['', Validators.required],
+      qtdPessoas: ['', [Validators.required, Validators.max(1000)]],
+      telefone: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      imagemURL: ['', Validators.required],
+    });
+  }
+
+  resetForm(): void{
+    this.frmEventoDetalhe.reset();
+  }
 }

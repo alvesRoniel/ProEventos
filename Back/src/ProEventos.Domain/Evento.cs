711f4bf73ef72eecdac5a2a0564975ea50d9668f
alvesRoniel <roniel.alves@gmail.com>
2021-05-24 20:07:19 -0300

    Seção 9: .NET Core - DTOs e Data Annotations - P5 - Validações do Evento com Data Annotation.

--- Back/src/ProEventos.Domain/Evento.cs
+++ Back/src/ProEventos.Domain/Evento.cs
@@ -1,17 +1,36 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ProEventos.Domain
 {
+    [Table("Eventos")]
     public class Evento
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(250)]
         public string Local { get; set; }
+
+        [Required]
         public DateTime? DataEvento { get; set; }
+        
+        [Required]
+        [MaxLength(250)]
         public string Tema { get; set; }
+
+        [Required]
         public int QtdPessoas { get; set; }
         public string ImagemURL { get; set; }
+
+        [Required]
         public string Telefone { get; set; }
+
+        [Required]
+        [MaxLength(150)]
         public string Email { get; set; }
 
         //Chave estrangeira
